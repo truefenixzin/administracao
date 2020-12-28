9b096a9f0f727bf921809f2f7a18dcc71ad54f68
Rodrigo dos Santos <rodrigodossantos8454@gmail.com>
2020-12-28 11:25:19 -0300

    nome do login

--- resources/views/admin/master/master_admin.blade.php
+++ resources/views/admin/master/master_admin.blade.php
@@ -45,7 +45,7 @@
         <a href="" class="brand-link">
             <img src="{{asset('back/dist/img/avatar.png')}}" alt="" class="brand-image img-circle elevation-3"
                  style="opacity: .8">
-            <span class="brand-text font-weight-light">SantosRSystems</span>
+            <span class="brand-text font-weight-light">{{{\Illuminate\Support\Facades\Auth::user()->name}}}</span>
         </a>
 
         <!-- Sidebar -->
