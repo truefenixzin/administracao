6ec83223b37db9cc9a2b9b5173af6ad6dc069858
rodrigo dos santos <rodrigodossantos8454@gmail.com>
2021-05-18 18:11:18 -0300

    pherfil

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -66,14 +66,11 @@
 
         <div class="row featurette">
             <div class="col-md-7">
-                <h2 class="featurette-heading">Primeiro título de featurezinhas. <span class="text-muted">Supreendente, não?!</span>
-                </h2>
-                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis
-                    euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus,
-                    tellus ac cursus commodo.</p>
+                <h2 class="featurette-heading"><span>Vem novidades por aí. Aguarde!</span></h2>
+                <p class="lead">Estamos melhorando ainda mais a nossa intranet, em breve teremos novidades.</p>
             </div>
             <div class="col-md-5">
-                <img src="{{\Illuminate\Support\Facades\Storage::url($slide    ->cover)}}" class="featurette-image img-fluid mx-auto"
+                <img src="{{asset('assets/brand/img/features.png')}}" class="featurette-image img-fluid mx-auto"
                      data-src="holder.js/500x500/auto"
                      alt="Generic placeholder image">
             </div>
@@ -81,21 +78,21 @@
 
         <hr class="featurette-divider">
 
-        <div class="row featurette">
-            <div class="col-md-7 order-md-2">
-                <h2 class="featurette-heading">Aêêê, moleque! <span class="text-muted">Tá legal ou não tá?</span></h2>
-                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis
-                    euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus,
-                    tellus ac cursus commodo.</p>
-            </div>
-            <div class="col-md-5 order-md-1">
-                <img src="{{asset('assets/brand/img/values-2.png')}}" class="featurette-image img-fluid mx-auto"
-                     data-src="holder.js/500x500/auto"
-                     alt="Generic placeholder image">
-            </div>
-        </div>
+{{--        <div class="row featurette">--}}
+{{--            <div class="col-md-7 order-md-2">--}}
+{{--                <h2 class="featurette-heading">Aêêê, moleque! <span class="text-muted">Tá legal ou não tá?</span></h2>--}}
+{{--                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis--}}
+{{--                    euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus,--}}
+{{--                    tellus ac cursus commodo.</p>--}}
+{{--            </div>--}}
+{{--            <div class="col-md-5 order-md-1">--}}
+{{--                <img src="{{asset('assets/brand/img/values-2.png')}}" class="featurette-image img-fluid mx-auto"--}}
+{{--                     data-src="holder.js/500x500/auto"--}}
+{{--                     alt="Generic placeholder image">--}}
+{{--            </div>--}}
+{{--        </div>--}}
 
-        <hr class="featurette-divider">
+{{--        <hr class="featurette-divider">--}}
 
 
     </div><!-- /.container -->
