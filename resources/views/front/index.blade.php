98106970f1a576b98b94ccd280a41669faa4f268
rodrigo dos santos <rodrigodossantos8454@gmail.com>
2021-05-18 18:41:19 -0300

    pherfil2

--- resources/views/front/index.blade.php
+++ resources/views/front/index.blade.php
@@ -1,5 +1,11 @@
 @extends('front.master.master')
 @section('content')
+    <div class="container marketing">
+        <div class="row">
+            <div class="col-12"></div>
+        </div>
+    </div>
+
 
     <!-- Inicio do carousel-->
     <div id="myCarousel" class="carousel slide" data-bs-ride="carousel">
@@ -78,21 +84,21 @@
 
         <hr class="featurette-divider">
 
-{{--        <div class="row featurette">--}}
-{{--            <div class="col-md-7 order-md-2">--}}
-{{--                <h2 class="featurette-heading">Aêêê, moleque! <span class="text-muted">Tá legal ou não tá?</span></h2>--}}
-{{--                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis--}}
-{{--                    euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus,--}}
-{{--                    tellus ac cursus commodo.</p>--}}
-{{--            </div>--}}
-{{--            <div class="col-md-5 order-md-1">--}}
-{{--                <img src="{{asset('assets/brand/img/values-2.png')}}" class="featurette-image img-fluid mx-auto"--}}
-{{--                     data-src="holder.js/500x500/auto"--}}
-{{--                     alt="Generic placeholder image">--}}
-{{--            </div>--}}
-{{--        </div>--}}
+        {{--        <div class="row featurette">--}}
+        {{--            <div class="col-md-7 order-md-2">--}}
+        {{--                <h2 class="featurette-heading">Aêêê, moleque! <span class="text-muted">Tá legal ou não tá?</span></h2>--}}
+        {{--                <p class="lead">Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis--}}
+        {{--                    euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus,--}}
+        {{--                    tellus ac cursus commodo.</p>--}}
+        {{--            </div>--}}
+        {{--            <div class="col-md-5 order-md-1">--}}
+        {{--                <img src="{{asset('assets/brand/img/values-2.png')}}" class="featurette-image img-fluid mx-auto"--}}
+        {{--                     data-src="holder.js/500x500/auto"--}}
+        {{--                     alt="Generic placeholder image">--}}
+        {{--            </div>--}}
+        {{--        </div>--}}
 
-{{--        <hr class="featurette-divider">--}}
+        {{--        <hr class="featurette-divider">--}}
 
 
     </div><!-- /.container -->
