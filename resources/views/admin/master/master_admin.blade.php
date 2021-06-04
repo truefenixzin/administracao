e4b919a8354cd9e4b33ee1bd6d18b098a2ab4b5c
rodrigo dos santos <rodrigodossantos8454@gmail.com>
2021-06-04 10:01:13 -0300

    ajuste logo menu

--- resources/views/admin/master/master_admin.blade.php
+++ resources/views/admin/master/master_admin.blade.php
@@ -43,7 +43,7 @@
     <aside class="main-sidebar sidebar-dark-primary elevation-4">
         <!-- Brand Logo -->
         <a class="brand-link">
-            <img src="{{asset('back/dist/img/avatar.png')}}" alt="" class="brand-image img-circle elevation-3"
+            <img src="{{asset('assets/brand/logo.png')}}" alt="" class="brand-image img-circle elevation-3"
                  style="opacity: .8">
             <span class="brand-text font-weight-light">{{{\Illuminate\Support\Facades\Auth::user()->name}}}</span>
         </a>
